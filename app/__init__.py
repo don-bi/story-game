fdb95850c3bcb0936932ab954d8cf04368b38c31
don-bi <dbi30@stuy.edu>
2022-11-07 14:00:51 -0500

    starting files

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,session,request
+import secrets
+
+app = Flask(__name__)
+
+app.secret_key = secrets.token_bytes(32)
+
+@app.route('/', methods=['POST'])
+def home():
+    if 'username' in session:
+        return render_template('discover.html',) # page with all the stories
+    return render_template('index.html',) # welcome page, has login button
+
+@app.route('/login', methods=['POST'])
